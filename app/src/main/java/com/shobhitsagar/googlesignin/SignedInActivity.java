4861911625b4ee13058ea9b7a4d8f07ae97e12c0
ShobhitSagar <shobhitsagar109@gmail.com>
2017-09-03 23:39:24 +0530

    First commit

---
+++ app/src/main/java/com/shobhitsagar/googlesignin/SignedInActivity.java
@@ -0,0 +1,17 @@
+package com.shobhitsagar.googlesignin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shobhitsagar on 03/09/17.
+ */
+
+public class SignedInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signedin);
+    }
+}
