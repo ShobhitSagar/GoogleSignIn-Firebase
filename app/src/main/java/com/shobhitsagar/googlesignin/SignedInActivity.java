bf6a087812a4b5f136df28aeaeff438912831b10
ShobhitSagar <shobhitsagar109@gmail.com>
2017-09-04 12:03:34 +0530

    Added AlertDialog when exit

--- app/src/main/java/com/shobhitsagar/googlesignin/SignedInActivity.java
+++ app/src/main/java/com/shobhitsagar/googlesignin/SignedInActivity.java
@@ -1,17 +1,48 @@
 package com.shobhitsagar.googlesignin;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
 
 /**
  * Created by shobhitsagar on 03/09/17.
  */
 
-public class SignedInActivity extends AppCompatActivity {
+public class SignedInActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signedin);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(SignedInActivity.this);
+        builder.setTitle("Exit");
+        builder.setIcon(R.drawable.ic_exit_to_app_black_24dp);
+        builder.setMessage("Are you sure you want to exit?");
+        builder.setCancelable(true);
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
